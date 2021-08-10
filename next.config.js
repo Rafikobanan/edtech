53f52e8204131879c08b77a7580773aab5ea5a56
Semyon <rafikobanan@gmail.com>
2021-08-10 20:34:53 +0500

    Add global sass

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
+const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 const nextConfig = {
-  pageExtensions: ['jsx', 'js', 'ts', 'tsx']
+  pageExtensions: ['jsx', 'js', 'ts', 'tsx'],
+  sassOptions: {
+    includePaths: [path.join(__dirname)],
+    prependData: `
+      @import "./styles/variables.scss";
+      @import "./styles/mixins.scss";
+    `
+  }
 };
 
 module.exports = withPlugins(
   [
     [
-      optimizedImages,
-      {
-        optimizeImages: false
-      }
+      optimizedImages
     ]
   ],
   nextConfig
